241220c3cc0363adb704e92d9d63ba0c3c49d3bf
shakthishetty <shakthishetty154@gmail.com>
2025-08-07 12:59:27 +0530

    logout functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import { isAuthenticated } from '@/lib/actions/auth.action'
 import LogoutButton from '@/components/LogoutButton'
+import { isAuthenticated } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
